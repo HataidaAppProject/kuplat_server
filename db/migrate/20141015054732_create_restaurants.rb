c47b1d2cb24b32a92a4fdfc5b826660490fe7dd8
taketo957 <taketo957@gmail.com>
2014-10-15 14:47:55 +0900

    レストランの追加

---
+++ db/migrate/20141015054732_create_restaurants.rb
@@ -0,0 +1,17 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.integer :type
+      t.float :rate
+      t.string :address
+      t.string :image_path
+      t.string :business_hours
+      t.string :phone_number
+      t.string :regular_holiday
+      t.string :link
+
+      t.timestamps
+    end
+  end
+end
