d5e627502fa07104103cdb95abcf2ab4030b38cc
taketo957 <taketo957@gmail.com>
2014-10-25 18:04:19 +0900

    指定ユーザのプロフィール取得

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,8 +1,18 @@
 class API < Grape::API
+  prefix :api
   version 'v1', using: :path
   format :json
-  prefix :api
-
   # 以下にAPIを書いていく
-
+  resource 'users' do
+    # /api/v1/users/:id
+    desc 'show user profile'
+    params do
+      requires :id, type: Integer, desc: "User ID"
+    end
+    route_param :id do
+      get do
+        Page.where(params[:id])
+      end
+    end
+  end
 end
