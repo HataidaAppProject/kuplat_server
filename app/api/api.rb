ead1253fc086258f1e7b0cbafcf005d61f61e26d
taketo957 <taketo957@gmail.com>
2014-10-29 13:17:35 +0900

    やることのコメントを追記

--- app/api/api.rb
+++ app/api/api.rb
@@ -3,7 +3,35 @@ class API < Grape::API
   version 'v1', using: :path
   format :json
   # 以下にAPIを書いていく
+  # ホーム画面用のAPI
+
+  # サークル・部活イベント一覧取得
+
+  # 就活イベント一覧取得
+
+  # セミナー・他イベント取得
+
+  # 指定イベント取得
+
+  # カフェ一覧取得
+
+  # ラーメン一覧取得
+
+  # レストラン一覧取得
+
+  # パン・スイーツ一覧取得
+
+  # 居酒屋・バー一覧取得
+
+  # 指定飲食店詳細取得
+
+  # 位置情報での検索結果取得
+
+  # ユーザの周辺のイベントなどの一覧取得
+
+  # ユーザ周り
   resource 'users' do
+    # ユーザの詳細取得（ユーザに紐付けられた飲食店，イベントも一緒に渡す）
     # /api/v1/users/:id
     desc 'show user profile'
     params do
@@ -14,5 +42,8 @@ class API < Grape::API
         Page.where(params[:id])
       end
     end
+
   end
+
+  # 認証関連のAPI
 end
