03548ba9ec4cb8fc193879f873eeee7e96478634
taketo957 <taketo957@gmail.com>
2014-11-04 23:35:53 +0900

    認証周りの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,24 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
   has_many :events, :through => :favorites
-  has_many :favorites, :order => 'created_at desc'
+  has_many :favorites, -> { order 'created_at desc'}
   has_many :restaurants, :through => :reviews
-  has_many :reviews, :order => 'created_at desc'
+  has_many :reviews, -> { order 'created_at desc'}
+
+  before_save :ensure_authentication_token
+
+  def ensure_authentication_token
+    if authentication_token.blank?
+      self.authentication_token = generate_authentication_token
+    end
+  end
+
+  private
+
+  def generate_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless User.where(authentication_token: token).first
+    end
+  end
 end
